9a2fcc6cf65e8cea162e68966ab7768e83c8a892
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-02-17 23:07:43 +0530

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import AuthContext from './context/AuthContext.jsx'
 import TaskContext from './context/TaskContext.jsx'
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthContext>
