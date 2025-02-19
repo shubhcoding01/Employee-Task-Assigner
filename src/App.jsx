6840f1e7d63362a760d9b58d5ffd861aefa2db2e
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-02-19 12:22:45 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -18,11 +18,15 @@ import { getLocalStorage, setLocalStorage } from './utils/localStorage'
   const handleLogin = (email,password) => {
     // console.log(email,password)
     if(email == 'admin12@mail.com' && password == '123'){
-      console.log("This is Admin")
+      setUser('admin')
+      console.log(user)
+      // console.log("This is Admin")
   }
   else if(email == 'user12@mail.com' && password =='123')
   {
-    console.log("This is User")
+    setUser('employee')
+    console.log(user)
+    // console.log("This is User")
   }
   else{
     alert("Invalid Email or Password")
