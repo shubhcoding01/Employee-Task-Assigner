1668566f83be635f55797e4be348fbc3534aa742
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-02-21 08:27:38 +0530

    Update

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -4,9 +4,10 @@ import NumberOfTask from '../others/NumberOfTask'
 import ListTask from '../TaskList/ListTask'
 
 const EmployeeDashboard = ({data}) => {
-  console.log(data)
+  // console.log(data)
   return (
     <div className='p-10 bg-[#1C1C1C] h-screen'>
+      <h1>{data.id}</h1>
       <Header />
       <NumberOfTask />
       <ListTask />
