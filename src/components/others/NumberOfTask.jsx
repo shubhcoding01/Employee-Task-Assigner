2497fb53a0f0c8bf450558d3583a898ed81f3432
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-02-22 08:08:10 +0530

    Update

--- src/components/others/NumberOfTask.jsx
+++ src/components/others/NumberOfTask.jsx
@@ -9,13 +9,13 @@ const NumberOfTask = ({data}) => {
             </div>
 
             <div  className='bg-blue-400 py-6 px-9 rounded-xl w-[45%]'>
-                    <h2 className='text-3xl font-semibold text-black'>0</h2>
-                    <h3 className='text-xl font-medium text-black'>New Task</h3>
+                    <h2 className='text-3xl font-semibold text-black'>{data.task_numbers.completed}</h2>
+                    <h3 className='text-xl font-medium text-black'>Completed Task</h3>
             </div>
 
             <div  className='bg-green-400 py-6 px-9 rounded-xl w-[45%]'>
                     <h2 className='text-3xl font-semibold text-black'>0</h2>
-                    <h3 className='text-xl font-medium text-black'>New Task</h3>
+                    <h3 className='text-xl font-medium text-black'>Activeted Task</h3>
             </div>
 
             <div  className='bg-yellow-200 py-6 px-9 rounded-xl w-[45%]'>
