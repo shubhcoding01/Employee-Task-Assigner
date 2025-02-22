3fe77d5a9485beadda552f37b5e2e84eceff0c88
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-02-22 08:10:20 +0530

    Update

--- src/components/others/NumberOfTask.jsx
+++ src/components/others/NumberOfTask.jsx
@@ -14,7 +14,7 @@ const NumberOfTask = ({data}) => {
             </div>
 
             <div  className='bg-green-400 py-6 px-9 rounded-xl w-[45%]'>
-                    <h2 className='text-3xl font-semibold text-black'>0</h2>
+                    <h2 className='text-3xl font-semibold text-black'>{data.task_numbers.active}</h2>
                     <h3 className='text-xl font-medium text-black'>Activeted Task</h3>
             </div>
 
