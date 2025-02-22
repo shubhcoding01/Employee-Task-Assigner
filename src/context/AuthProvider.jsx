7f7a0cc40f7714e845314ab6cacc033666fdeb69
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-02-22 07:53:33 +0530

    Update

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react'
-import { getLocalStorage } from '../utils/localStorage'
+import { getLocalStorage, setLocalStorage } from '../utils/localStorage'
 
 
 export const AuthContext = createContext() 
@@ -9,6 +9,7 @@ const AuthProvider = ({children}) => {
   const [userData, setUserData] = useState(null)
 
   useEffect(() => {
+    setLocalStorage()
     const {employees,admin} = getLocalStorage()
     setUserData({employees,admin})
     
