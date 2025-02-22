d804f0f050263ff6151d081ae97d99571806207e
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-02-22 07:54:41 +0530

    Update

--- src/components/others/Header.jsx
+++ src/components/others/Header.jsx
@@ -4,7 +4,7 @@ const Header = ({data}) => {
   // console.log(data)
   return (
     <div className='flex justify-between items-end bg-emerald-600 text-white'>
-        <h1 className='text-2xl'>Hello <br /><span className='text-3xl font-semibold'>Shubham👋</span></h1>
+        <h1 className='text-2xl'>Hello <br /><span className='text-3xl font-semibold'>{data.firstname}👋</span></h1>
         <button className='bg-red-500 text-white px-5 py-2 rounded-lg text-lg font-medium'>Log out</button>
     </div>
   )
