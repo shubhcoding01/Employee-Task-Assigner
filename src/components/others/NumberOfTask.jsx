fbccf54cc6454d3f0121a068cf32684bcd7f844b
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-02-22 08:06:36 +0530

    Update

--- src/components/others/NumberOfTask.jsx
+++ src/components/others/NumberOfTask.jsx
@@ -4,7 +4,7 @@ const NumberOfTask = ({data}) => {
   return (
     <div className=' mt-10 flex justify-between screen gap-5'>
             <div  className='bg-red-400 py-6 px-9 rounded-xl w-[45%]'>
-                    <h2 className='text-3xl font-semibold text-black'>0</h2>
+                    <h2 className='text-3xl font-semibold text-black'>{data.task_numbers.new}</h2>
                     <h3 className='text-xl font-medium text-black'>New Task</h3>
             </div>
 
