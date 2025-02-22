7d9085c026f4a90e716aa16892083c5590508e2d
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-02-22 08:32:12 +0530

    Update

--- src/components/TaskList/ListTask.jsx
+++ src/components/TaskList/ListTask.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import AcceptTask from './AcceptTask'
 
 const ListTask = ({data}) => {
   return (
     <div id='listtask' className='overflow-x-auto mt-7 h-[55%] w-full py-5 flex justify-start items-center gap-5 flex-nowrap'>
-        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-red-300 rounded-xl'>
+      <AcceptTask />
+        {/* <div className='flex-shrink-0 h-full w-[300px] p-5 bg-red-300 rounded-xl'>
           <div className='flex justify-between items-center'>
             <h3 className='bg-red-500 rounded px-3 py-1 text-black text-sm'>High</h3>
             <h4 className='text-black font-bold'>14Feb 2025</h4>
@@ -11,7 +13,7 @@ const ListTask = ({data}) => {
           <h2 className='mt-5 font-semibold text-violet-950 text-2xl'>Make A YouTube Video</h2>
           <p className='mt-2 text-sm text-blue-800'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
             Quae enim blanditiis id, perferendis aspernatur voluptatum?</p>
-        </div>
+        </div> */}
         <div className='flex-shrink-0 h-full w-[300px] p-5 bg-yellow-300 rounded-xl'>
         <div className='flex justify-between items-center'>
             <h3 className='bg-red-500 rounded px-3 py-1 text-black text-sm'>High</h3>
