5a68047a615c1adf6e4f7fac4c293bf7bc001910
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-02-24 21:10:33 +0530

    Update

--- src/components/others/Header.jsx
+++ src/components/others/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Header = ({data}) => {
   // console.log(data)
   return (
-    <div className='flex justify-between items-end bg-emerald-600 text-white'>
-        <h1 className='text-2xl'>Hello <br /><span className='text-3xl font-semibold'>{data.firstname}👋</span></h1>
+    <div className='flex justify-between items-end  text-white'>
+        <h1 className='text-2xl text-lime-500 '>Hello <br /><span className='text-3xl font-semibold text-lime-300'>{data.firstname}👋</span></h1>
         <button className='bg-red-500 text-white px-5 py-2 rounded-lg text-lg font-medium'>Log out</button>
     </div>
   )
--- src/components/others/NumberOfTask.jsx
+++ src/components/others/NumberOfTask.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const NumberOfTask = ({data}) => {
   return (
     <div className=' mt-10 flex justify-between screen gap-5'>
-            <div  className='bg-red-400 py-6 px-9 rounded-xl w-[45%]'>
+            <div  className=' py-6 px-9 rounded-xl w-[45%] bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400'>
                     <h2 className='text-3xl font-semibold text-black'>{data.task_numbers.new}</h2>
                     <h3 className='text-xl font-medium text-black'>New Task</h3>
             </div>
 
-            <div  className='bg-blue-400 py-6 px-9 rounded-xl w-[45%]'>
+            <div  className='bg-blue-400 py-6 px-9 rounded-xl w-[45%] bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400'>
                     <h2 className='text-3xl font-semibold text-black'>{data.task_numbers.completed}</h2>
                     <h3 className='text-xl font-medium text-black'>Completed Task</h3>
             </div>
