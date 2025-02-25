7ca7a58a30b4504e1df3d0dbf5fa8be730a26d57
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-02-25 15:34:12 +0530

    Update

--- src/components/others/Header.jsx
+++ src/components/others/Header.jsx
@@ -12,10 +12,15 @@ const Header = () => {
   //   setUsername(data.firstname)
   // }
   
+    const logoutUser = ()=>
+    {
+
+    }
+
   return (
     <div className='flex justify-between items-end  text-white'>
         <h1 className='text-2xl text-lime-500 '>Hello <br /><span className='text-3xl font-semibold text-lime-300'>username👋</span></h1>
-        <button className='bg-red-500 text-white px-5 py-2 rounded-lg text-lg font-medium'>Log out</button>
+        <button onClick={logoutUser} className='bg-red-500 text-white px-5 py-2 rounded-lg text-lg font-medium'>Log out</button>
     </div>
   )
 }
