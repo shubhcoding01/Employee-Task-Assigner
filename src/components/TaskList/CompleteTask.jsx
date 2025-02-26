47aec48f617bd725d5bba3a0a1ed13a3f7fd2cbc
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-02-26 10:33:09 +0530

    Update

--- src/components/TaskList/CompleteTask.jsx
+++ src/components/TaskList/CompleteTask.jsx
@@ -4,7 +4,7 @@ const CompleteTask = () => {
   return (
     <div className='flex-shrink-0 h-full w-[300px] p-5 bg-green-300 rounded-xl'>
         <div className='flex justify-between items-center'>
-            <h3 className='bg-red-500 rounded px-3 py-1 text-black text-sm'>High</h3>
+            <h3 className='bg-red-500 rounded px-3 py-1 text-black text-sm'>{data.category}</h3>
             <h4 className='text-black font-bold'>{data.task_date}</h4>
           </div>
           <h2 className='mt-5 font-semibold text-violet-950 text-2xl'>{data.task_title}</h2>
