e055f92b4ac2458954a7da91ed601b62798cb43d
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-02-26 10:08:27 +0530

    Update

--- src/components/TaskList/AcceptTask.jsx
+++ src/components/TaskList/AcceptTask.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 const AcceptTask = ({data}) => {
+  console.log(data);
+  
   return (
     <div className='flex-shrink-0 h-full w-[300px] p-5 bg-red-300 rounded-xl'>
           <div className='flex justify-between items-center'>
