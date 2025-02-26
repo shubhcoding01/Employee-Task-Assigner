770c5fa34c991b1dae3103e78ea6f689da6b1197
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-02-26 10:28:54 +0530

    Update

--- src/components/TaskList/NewTask.jsx
+++ src/components/TaskList/NewTask.jsx
@@ -7,7 +7,7 @@ const NewTask = ({data}) => {
             <h3 className='bg-red-500 rounded px-3 py-1 text-black text-sm'>{data.category}</h3>
             <h4 className='text-black font-bold'>{data.task_date}</h4>
           </div>
-          <h2 className='mt-5 font-semibold text-violet-950 text-2xl'>Make A YouTube Video</h2>
+          <h2 className='mt-5 font-semibold text-violet-950 text-2xl'>{data.task_title}</h2>
           <p className='mt-2 text-sm text-blue-800'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
             Quae enim blanditiis id, perferendis aspernatur voluptatum?</p>
 
