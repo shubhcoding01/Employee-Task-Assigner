9d6f774e8fc7adc5a3a2a97bd67b2062a650de0a
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-02-26 11:56:33 +0530

    Update

--- src/components/others/AllTask.jsx
+++ src/components/others/AllTask.jsx
@@ -8,7 +8,7 @@ const AllTask = () => {
     
 
   return (
-    <div id='allTask' className='p-5 bg-[#1c1c1c] rounded mt-5 overflow-auto h-48'>
+    <div id='allTask' className='p-5 bg-[#1c1c1c] rounded mt-5  h-60'>
         <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
             <h2 className='w-1/5 bg-slate-500'>Employee Name</h2>
             <h3 className='w-1/5 bg-slate-500'>New Task</h3>
@@ -18,7 +18,7 @@ const AllTask = () => {
 
         </div>
         
-        <div>
+        <div id='allTask' className='h-[80%] overflow-auto'>
         {authData.employees.map(function (elem) {
             return <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
             <h2 className='w-1/5 bg-slate-500'>{elem.firstname}</h2>
