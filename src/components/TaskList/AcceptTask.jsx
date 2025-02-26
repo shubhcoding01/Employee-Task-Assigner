b4cb4f133186aa8b0db0abb06094b31100e0cc44
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-02-26 10:11:25 +0530

    Update

--- src/components/TaskList/AcceptTask.jsx
+++ src/components/TaskList/AcceptTask.jsx
@@ -9,7 +9,7 @@ const AcceptTask = ({data}) => {
             <h3 className='bg-red-500 rounded px-3 py-1 text-black text-sm'>High</h3>
             <h4 className='text-black font-bold'>14Feb 2025</h4>
           </div>
-          <h2 className='mt-5 font-semibold text-violet-950 text-2xl'>Make A YouTube Video</h2>
+          <h2 className='mt-5 font-semibold text-violet-950 text-2xl'>{data.task_title}</h2>
           <p className='mt-2 text-sm text-blue-800'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
             Quae enim blanditiis id, perferendis aspernatur voluptatum?</p>
 
