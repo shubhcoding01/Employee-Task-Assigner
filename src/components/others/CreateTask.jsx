25d25b8aaa2ac80005ab4bdb1dfff7a1b12ea25d
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-02-26 12:51:38 +0530

    Update

--- src/components/others/CreateTask.jsx
+++ src/components/others/CreateTask.jsx
@@ -24,22 +24,46 @@ const CreateTask = () => {
 
               <div>
                 <h3 className='text-sm text-gray-300 mb-0.5'>Task Title</h3>
-                <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='Make a UI Design' />
+                <input 
+                value={taskTitle}
+                onChange={(e) =>{
+                  setTaskTitle(e.target.value)
+                }}
+                className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='Make a UI Design' 
+                />
               </div>
 
               <div>   
                 <h3 className='text-sm text-gray-300 mb-0.5'>Date</h3>
-                <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="date" />
+                <input 
+                value={taskDate}
+                onChange={(e) =>{
+                  setTaskDate(e.target.value)
+                }}
+                className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="date" 
+                />
               </div>
 
               <div>
                 <h3 className='text-sm text-gray-300 mb-0.5'>Asign to</h3>
-                <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='Employee Name' />
+                <input 
+                value={asignTo}
+                onChange={(e) =>{
+                  setAsignTo(e.target.value)
+                }}
+                className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='Employee Name' 
+                />
               </div>
 
               <div>
                 <h3 className='text-sm text-gray-300 mb-0.5'>Category</h3>
-                <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='design, dev, etc' />
+                <input 
+                value={taskCategory}
+                onChange={(e) =>{
+                  setTaskCategory(e.target.value)
+                }}
+                className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='design, dev, etc' 
+                />
               </div>
 
               
@@ -48,7 +72,12 @@ const CreateTask = () => {
             
            <div className='w-2/5 flex flex-col items-start'>
                <h3 className='text-sm text-gray-300 mb-0.5'>Description</h3>
-               <textarea className='w-full h-44 text-sm py-2 px-4  rounded outline-none bg-transparent border-[1px] border-gray-400 ' name="" id="" cols="30" rows="10"></textarea>
+               <textarea 
+               value={taskDescription}
+               onChange={(e) =>{
+                 setTaskDescription(e.target.value)
+               }}
+               className='w-full h-44 text-sm py-2 px-4  rounded outline-none bg-transparent border-[1px] border-gray-400 ' name="" id="" cols="30" rows="10"></textarea>
                <button className='bg-emerald-500 py-3 hover:bg-emerald-600 px-5 rounded text-sm mt-4 w-full'>Create Task</button>
               
             </div>
