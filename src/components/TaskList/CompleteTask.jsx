1f591a28e13ef047247be579243aaf2b70bd9c69
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-02-26 10:31:19 +0530

    Update

--- src/components/TaskList/CompleteTask.jsx
+++ src/components/TaskList/CompleteTask.jsx
@@ -8,8 +8,7 @@ const CompleteTask = () => {
             <h4 className='text-black font-bold'>14Feb 2025</h4>
           </div>
           <h2 className='mt-5 font-semibold text-violet-950 text-2xl'>Make A YouTube Video</h2>
-          <p className='mt-2 text-sm text-blue-800'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Quae enim blanditiis id, perferendis aspernatur voluptatum?</p>
+          <p className='mt-2 text-sm text-blue-800'>{data.task_description}</p>
 
             <div className='mt-4'>
                 <button className='w-full bg-purple-600 rounded-lg text-black py-1 px-2 opacity-100 hover:bg-purple-800 hover:scale-110 transition-all duration-300'>Completed Task</button>
