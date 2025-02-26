87212132185a976c0fd5ce0edecba24aaf445937
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-02-26 10:24:37 +0530

    Update

--- src/components/TaskList/AcceptTask.jsx
+++ src/components/TaskList/AcceptTask.jsx
@@ -7,7 +7,7 @@ const AcceptTask = ({data}) => {
     <div className='flex-shrink-0 h-full w-[300px] p-5 bg-red-300 rounded-xl'>
           <div className='flex justify-between items-center'>
             <h3 className='bg-red-500 rounded px-3 py-1 text-black text-sm'>{data.category}</h3>
-            <h4 className='text-black font-bold'>14Feb 2025</h4>
+            <h4 className='text-black font-bold'>{data.task_date}</h4>
           </div>
           <h2 className='mt-5 font-semibold text-violet-950 text-2xl'>{data.task_title}</h2>
           <p className='mt-2 text-sm text-blue-800'>{data.task_description}</p>
