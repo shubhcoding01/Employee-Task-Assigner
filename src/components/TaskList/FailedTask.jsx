ea6f25875f68aa620d4cf23ae4c09c5ccfe9789e
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-02-26 10:34:26 +0530

    Update

--- src/components/TaskList/FailedTask.jsx
+++ src/components/TaskList/FailedTask.jsx
@@ -5,7 +5,7 @@ const FailedTask = () => {
     <div className='flex-shrink-0 h-full w-[300px] p-5 bg-blue-300 rounded-xl'>
         <div className='flex justify-between items-center'>
             <h3 className='bg-red-500 rounded px-3 py-1 text-black text-sm'>{data.category}</h3>
-            <h4 className='text-black font-bold'>14Feb 2025</h4>
+            <h4 className='text-black font-bold'>{data.task_date}</h4>
           </div>
           <h2 className='mt-5 font-semibold text-violet-950 text-2xl'>Make A YouTube Video</h2>
           <p className='mt-2 text-sm text-blue-800'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
