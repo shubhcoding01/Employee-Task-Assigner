3fd8c2b73c44aa17aaaca6beac90bccdf119549b
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-02-26 10:35:54 +0530

    Update

--- src/components/TaskList/FailedTask.jsx
+++ src/components/TaskList/FailedTask.jsx
@@ -8,8 +8,7 @@ const FailedTask = () => {
             <h4 className='text-black font-bold'>{data.task_date}</h4>
           </div>
           <h2 className='mt-5 font-semibold text-violet-950 text-2xl'>{data.task_title}</h2>
-          <p className='mt-2 text-sm text-blue-800'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-            Quae enim blanditiis id, perferendis aspernatur voluptatum?</p>
+          <p className='mt-2 text-sm text-blue-800'>{data.task_description}</p>
 
             <div className='mt-2'>
                 <button className='w-full bg-red-600 rounded-md' >Failed Task</button>
