46ebae8bd87a2ac6e03a2389298b04b742d2b8dd
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-02-26 10:33:49 +0530

    Update

--- src/components/TaskList/FailedTask.jsx
+++ src/components/TaskList/FailedTask.jsx
@@ -4,7 +4,7 @@ const FailedTask = () => {
   return (
     <div className='flex-shrink-0 h-full w-[300px] p-5 bg-blue-300 rounded-xl'>
         <div className='flex justify-between items-center'>
-            <h3 className='bg-red-500 rounded px-3 py-1 text-black text-sm'>High</h3>
+            <h3 className='bg-red-500 rounded px-3 py-1 text-black text-sm'>{data.category}</h3>
             <h4 className='text-black font-bold'>14Feb 2025</h4>
           </div>
           <h2 className='mt-5 font-semibold text-violet-950 text-2xl'>Make A YouTube Video</h2>
